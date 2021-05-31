b85adb557ac5c0e9a3966a027b7c06a78ec97164
ChitsanuphongCh <ChitsanuphongChaihong@outlook.com>
2021-05-31 22:06:50 +0700

    1.0.2

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>หอพักนักศึกษา มจพ. ปราจีนบุรี</title>
         <link rel="stylesheet" href="./assets/css/main.css">
-        <link rel="icon" href="./assets/img/logoKmutnb.png">
+        <link rel="icon" href="./assets/img/logoKmutnb.webp">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     </head>
 <body>
